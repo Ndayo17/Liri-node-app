3e65a5437be5bf57feac913c5ebed42bc80094e6
solomon <ndayo@hotmail.com>
2018-04-24 15:43:58 -0500

    unstaged file

---
+++ Keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+
+spotify Keys,
+Client ID 8b63b46524044fde91a77064cd05003c 
+Client Secret 75e5c97fcada40bc94e07a765ab217c4 
